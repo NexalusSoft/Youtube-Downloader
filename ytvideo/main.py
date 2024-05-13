a39d2532c45d30b7656554bdfe7abb10b372bd2a
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-05-13 17:19:54 +0530

    initialize params

--- ytvideo/main.py
+++ ytvideo/main.py
@@ -1,4 +1,7 @@
 class YoutubeDownloader:
+    video_url: str
+    save_path: str
+    
     def __init__(self, video_url: str, save_path: str) -> None:
         self.video_url = video_url
         self.save_path = save_path
