3aec14890ae59b0534b48d90ddf595dc9795523a
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-05-13 17:20:43 +0530

    add doc string

--- ytvideo/main.py
+++ ytvideo/main.py
@@ -1,7 +1,14 @@
 class YoutubeDownloader:
+    """
+    Class for downloading youtube videos
+
+    Args:
+        video_url(str): url of the video
+        save_path(str): path to save the video
+    """
     video_url: str
     save_path: str
-    
+
     def __init__(self, video_url: str, save_path: str) -> None:
         self.video_url = video_url
         self.save_path = save_path
