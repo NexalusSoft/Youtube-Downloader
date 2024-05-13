60165bc500c7c88f26d629b34ce7d9d6905656a0
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-05-13 17:19:11 +0530

    add types

--- ytvideo/main.py
+++ ytvideo/main.py
@@ -1,4 +1,4 @@
 class YoutubeDownloader:
-    def __init__(self, video_url, save_path):
+    def __init__(self, video_url: str, save_path: str) -> None:
         self.video_url = video_url
         self.save_path = save_path
