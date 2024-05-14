0dd32e4adc3192125a497743658c15ec0e21ab76
bhargavdesai36 <bhargavdesai36@gmail.com>
2024-05-14 15:22:01 +0530

    minor changes

--- ytvideo/main.py
+++ ytvideo/main.py
@@ -6,15 +6,15 @@ class YoutubeDownloader:
     Class for downloading youtube videos
 
     Args:
-        video_url(str): url of the video
-        save_path(str): path to save the video
+        url(str): url of the video
+        path(str): path to save the video
     """
-    video_url: str
-    save_path: str
+    url: str
+    path: str
 
-    def __init__(self, video_url: str, save_path: str) -> None:
-        self.video_url = video_url
-        self.save_path = save_path
+    def __init__(self, url: str, path: str) -> None:
+        self.video_url = url
+        self.save_path = path
 
         self.video_id = self.extract_video_id(self.video_url)
 
